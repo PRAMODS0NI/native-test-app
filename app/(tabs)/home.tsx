9e5e8b6d67761519ad341875c08e93d507e372e5
pramodsoni-jhc <pramodsoni@2584@gmail.com>
2025-01-08 20:10:35 +0530

    feat: :sparkles: Background audio added

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,13 +1,39 @@
-import { Text, StyleSheet, View } from 'react-native'
-import React, { Component } from 'react'
+import { Text, StyleSheet, View, Button } from 'react-native'
+import React, { useEffect } from 'react'
 import { Ionicons } from '@expo/vector-icons'
+import { Audio } from 'expo-av'
 
 export default function Home() {
-    return (
-      <View style={styles.container}>
-        <Ionicons name='logo-tux' size={200}/>
-      </View>
-    )
+  const [sound, setSound] = React.useState<Audio.Sound | null>(null);
+
+  async function loadSound() {
+    try {
+      const { sound } = await Audio.Sound.createAsync(
+        require('../../assets/got.mp3'),
+        { isLooping: true }
+      );
+      setSound(sound);
+      await sound.playAsync();
+    } catch (error) {
+      console.error('Error loading sound', error);
+    }
+  }
+
+  useEffect(() => {
+    loadSound();
+
+    return () => {
+      if (sound) {
+        sound.unloadAsync();    
+      }
+    };
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Ionicons name='logo-tux' size={200}/>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
